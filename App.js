9ba971b89617d8c67b3c291668623f4ee1a79735
Sheikh Hamad <sheikhamad1830@gmail.com>
2024-03-21 11:10:36 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+// import { View, Text } from 'react-native'
+import React from 'react';
+import H from './src/HHH';
+import Home from './src/screens/Home';
+
+export default function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
